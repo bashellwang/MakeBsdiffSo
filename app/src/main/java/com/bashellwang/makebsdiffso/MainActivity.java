5777d22583f0685379c48535b2d883c9bd6c6ebf
Liang Wang <liang.wang@shopee.com>
2020-04-24 10:44:03 +0800

    主页面两个功能按钮

--- app/src/main/java/com/bashellwang/makebsdiffso/MainActivity.java
+++ app/src/main/java/com/bashellwang/makebsdiffso/MainActivity.java
@@ -3,12 +3,35 @@ package com.bashellwang.makebsdiffso;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mMakeDiffBtn;
+    private Button mMergeDiffBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMakeDiffBtn = findViewById(R.id.make_diff);
+        mMergeDiffBtn = findViewById(R.id.merge_diff);
+
+        mMakeDiffBtn.setOnClickListener(this);
+        mMergeDiffBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.make_diff:
+                break;
+            case R.id.merge_diff:
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ readme.md
@@ -0,0 +1,5 @@
+bsdiff 4.3:http://www.daemonology.net/bsdiff/bsdiff-4.3.tar.gz
+
+依赖 bzip2:
+
+https://sourceforge.net/projects/bzip2/files/latest/download
